9ef234b3ae9cab1275bed6ea177ebc240750b596
manuel dellagala <manuel.dellagala@gmail.com>
2021-11-05 16:51:17 +0100

    migrations

--- giornalisti/news/models.py
+++ giornalisti/news/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Giornalista(models.Model):
+    nome = models.CharField(max_length=50)
+    conome = models.CharField(max_length=50)
+    def __str__(self):
+        return (f"{self.nome} {self.conome}")
+class Articolo(models.Model):
+
+    """ modello generico di articolo"""
+
+    titolo = models.CharField(max_length = 100)
+    contenuto = models.TextField
+    giornalista = models.ForeignKey(Giornalista,on_delete = models.CASCADE,related_name='articoli') 
+
+    def __str__(self):
+        return (f"{self.titolo}")
+    
