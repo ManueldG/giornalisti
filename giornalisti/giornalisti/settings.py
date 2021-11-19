45e2132d1dc4ef2974de19714edbd580cd27c418
manuel dellagala <manuel.dellagala@gmail.com>
2021-11-19 17:19:21 +0100

    inserito bootstrap

--- giornalisti/giornalisti/settings.py
+++ giornalisti/giornalisti/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'news'
+    
+    'bootstrap5',
+    
+    'news',
+    
 ]
 
 MIDDLEWARE = [
